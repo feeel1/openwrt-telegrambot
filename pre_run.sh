791bf052352af11a815626125800cd88ac413195
Stasinskii <Feeel1@yandex.ru>
2026-05-01 18:16:46 +0200

    Translate comments in pre_run.sh to Russian

--- pre_run.sh
+++ pre_run.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-# Ubah format file dari DOS ke UNIX
+# Изменение формата файлов из DOS в UNIX
 dos2unix /www/assisten/bot/run_bot.sh
 dos2unix /www/assisten/bot/update.sh
 dos2unix /www/assisten/bot/bot.py
 dos2unix /www/assisten/bot/restart.sh
 
-# Tambahkan izin eksekusi
+# Добавление прав на выполнение
 chmod +x /www/assisten/bot/run_bot.sh
 chmod +x /www/assisten/bot/update.sh
 chmod +x /www/assisten/bot/bot.py
 chmod +x /www/assisten/bot/restart.sh
 
-echo "Persiapan file selesai."
+echo "Подготовка файлов завершена."
